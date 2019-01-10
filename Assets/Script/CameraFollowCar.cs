e4ee5906c4d2d0f70bfc15164a3b4a29c0f2bf11
PierreMiola <pierre.miola@gmail.com>
2019-01-10 11:18:35 +0100

    deplacements car

---
+++ Assets/Script/CameraFollowCar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowCar : MonoBehaviour {
+
+    //public GameObject car;
+
+   //private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+       //offset = transform.position - car.trasform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = car.transform.position + offset;
+	}
+}
---
+++ Assets/Script/moveCar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCar : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+    public float rotateSpeed = 30f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+            transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -rotateSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -moveSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
+        }
+    }
+}
