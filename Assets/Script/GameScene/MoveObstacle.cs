d5e50a1e35b71bc415d5e6b18d6b664c1d96cf1c
PierreMiola <pierre.miola@gmail.com>
2019-02-03 22:55:59 +0100

    ajout obstacles

---
+++ Assets/Script/GameScene/MoveObstacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour {
+
+    private float cptX;
+    private float min;
+    private float max;
+    private bool b;
+
+    // Use this for initialization
+    void Start () {
+        min = cptX - 3;
+        max = cptX + 3;
+        b = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (cptX <= min || cptX >= max)
+        {
+            b = !b;
+        }
+        if (b)
+        {
+            cptX += 0.2f;
+            this.transform.position = new Vector3(this.transform.position.x + 0.2f, this.transform.position.y, this.transform.position.z);
+        }
+        else
+        {
+            cptX -= 0.2f;
+            this.transform.position = new Vector3(this.transform.position.x - 0.2f, this.transform.position.y, this.transform.position.z);
+        }
+
+    }
+}
