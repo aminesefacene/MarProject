7ab95bb3ffa8446316ef9139a0ebfcf0936ac1f0
PierreMiola <pierre.miola@gmail.com>
2019-02-04 08:22:02 +0100

    ajout vitesse car

---
+++ Assets/Script/GameScene/SpeedValue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedValue : MonoBehaviour {
+
+    public Text t = null;
+    private GameObject car;
+    private Vector3 lastPosition;
+    private float speed;
+
+    // Use this for initialization
+    void Start () {
+        car = GameObject.Find("car");
+        lastPosition = car.GetComponent<Rigidbody>().transform.position;
+    }
+
+
+    void FixedUpdate()
+    {
+        speed = (transform.position - lastPosition).magnitude*100;
+        t.text = speed.ToString();
+        lastPosition = transform.position;
+    }
+}
